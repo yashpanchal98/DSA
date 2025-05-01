6ee2f051a3d4373e2527aa27f3a899c58f22708a
yashpanchal98 <pclyash@gmail.com>
2025-05-01 16:35:44 +0530

    Time: 1 ms (81.92%) | Memory: 42.7 MB (94.25%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.java
@@ -0,0 +1,30 @@
+class Solution {
+
+    public void helper( List<String> list,StringBuilder sb,int n,int open, int close){
+
+        // base case
+        if(open == n && close == n){
+            list.add(sb.toString());
+        }
+
+        if(open > n || close > open) return;
+
+        sb.append('(');
+        helper(list,sb,n,open+1,close);
+        sb.deleteCharAt(sb.length()-1);
+
+        sb.append(')');
+        helper(list,sb,n,open,close+1);
+        sb.deleteCharAt(sb.length()-1);
+
+    }
+
+    public List<String> generateParenthesis(int n) {
+        
+        List<String> list = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        helper(list, sb, n, 0, 0);
+        return list;
+        
+    }
+}
