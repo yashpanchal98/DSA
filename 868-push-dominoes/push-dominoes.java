845f2c26d07cba0cc98690dc3f04cb37fe2ff6df
yashpanchal98 <pclyash@gmail.com>
2025-05-02 18:32:08 +0530

    Time: 28 ms (30.94%) | Memory: 45.8 MB (47.55%) - LeetSync

--- 868-push-dominoes/push-dominoes.java
+++ 868-push-dominoes/push-dominoes.java
@@ -9,13 +9,9 @@ class Solution {
         // Calculate right forces
         for (int i = 0; i < n; i++) {
             char ch = dominoes.charAt(i);
-            if (ch == 'R') {
-                idx = i;
-            } else if (ch == 'L') {
-                idx = -1;
-            } else if (idx != -1) {
-                rightF[i] = i - idx;
-            }
+            if (ch == 'R')  idx = i;
+            else if (ch == 'L') idx = -1;
+            else if (idx != -1) rightF[i] = i - idx;
         }
 
         int[] leftF = new int[n];
@@ -24,30 +20,19 @@ class Solution {
         // Calculate left forces
         for (int i = n - 1; i >= 0; i--) {
             char ch = dominoes.charAt(i);
-            if (ch == 'L') {
-                idx = i;
-            } else if (ch == 'R') {
-                idx = -1;
-            } else if (idx != -1) {
-                leftF[i] = idx - i;
-            }
+            if (ch == 'L') idx = i;
+            else if (ch == 'R') idx = -1;
+            else if (idx != -1)   leftF[i] = idx - i;
         }
 
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++) {
-            if (rightF[i] == -1 && leftF[i] == -1) {
-                sb.append(dominoes.charAt(i));
-            } else if (rightF[i] == -1) {
-                sb.append('L');
-            } else if (leftF[i] == -1) {
-                sb.append('R');
-            } else if (rightF[i] == leftF[i]) {
-                sb.append('.');
-            } else if (rightF[i] < leftF[i]) {
-                sb.append('R');
-            } else {
-                sb.append('L');
-            }
+            if (rightF[i] == -1 && leftF[i] == -1) sb.append(dominoes.charAt(i));
+            else if (rightF[i] == -1) sb.append('L');
+            else if (leftF[i] == -1) sb.append('R');
+            else if (rightF[i] == leftF[i]) sb.append('.');
+            else if (rightF[i] < leftF[i]) sb.append('R');
+            else  sb.append('L');
         }
 
         return sb.toString();
