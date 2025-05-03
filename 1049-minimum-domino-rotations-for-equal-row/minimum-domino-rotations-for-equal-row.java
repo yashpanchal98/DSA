18a276af08f044bb97d73a46be04eaf15df6be5d
yashpanchal98 <pclyash@gmail.com>
2025-05-03 11:11:53 +0530

    Time: 5 ms (54.93%) | Memory: 50.2 MB (84.80%) - LeetSync

--- 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.java
+++ 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.java
@@ -6,18 +6,18 @@ class Solution {
         int bottom[] = new int[7];
         int same[] = new int[7];
         for (int i = 0; i < n; i++) {
-           
-                top[tops[i]]++;
-                bottom[bottoms[i]]++;
-                if(tops[i]==bottoms[i]){
-                    same[tops[i]]++;
-                }
+
+            top[tops[i]]++;
+            bottom[bottoms[i]]++;
+            if (tops[i] == bottoms[i])
+                same[tops[i]]++;
+
         }
         int ans = -1;
 
         for (int i = 0; i < 7; i++) {
             if (top[i] + bottom[i] - same[i] >= n) {
-                ans = Math.min(top[i]-same[i], bottom[i]-same[i]);
+                ans = Math.min(top[i] - same[i], bottom[i] - same[i]);
             }
         }
         return ans;
