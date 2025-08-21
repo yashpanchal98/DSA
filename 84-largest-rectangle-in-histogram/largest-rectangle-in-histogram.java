12f37bd43cbcdf44f38c6f92a1f2e722e2c2ca32
yashpanchal98 <pclyash@gmail.com>
2025-08-22 02:33:57 +0530

    Time: 73 ms (47.01%) | Memory: 61.1 MB (5.83%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,41 +1,20 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
         
-        Stack<Integer> st = new Stack<>();
-
+        Stack<Integer> stack = new Stack<>();
+        int maxArea = 0;
         int n = heights.length;
-        int nextSmaller[] = new int[n];
+        int[] h = Arrays.copyOf(heights, n + 1);
 
-        for(int i=n-1; i>=0; i--){
-            while(!st.isEmpty() && heights[st.peek()]>=heights[i]){
-                st.pop();
+        for (int i = 0; i <= n; i++) {
+            while (!stack.isEmpty() && h[i] < h[stack.peek()]) {
+                int height = h[stack.pop()];
+                int width = stack.isEmpty() ? i : i - stack.peek() - 1;
+                maxArea = Math.max(maxArea, height * width);
             }
-            if(st.isEmpty()) nextSmaller[i] = n;
-            else nextSmaller[i] = st.peek();
-            st.push(i);
-            System.out.print(st.peek());
+            stack.push(i);
         }
 
-        st.clear();
-
-        int prevSmaller[] = new int[n];
-
-        for(int i=0; i<n; i++){
-
-            while(!st.isEmpty() && heights[st.peek()]>=heights[i]){
-                st.pop();
-            }
-            if(st.isEmpty()) prevSmaller[i] = -1;
-            else prevSmaller[i] = st.peek();
-            st.push(i);
-        }
-        int ans = 0;
-        for(int i=0; i<n; i++){
-            int width = nextSmaller[i]-prevSmaller[i]-1;
-            int ht = heights[i];
-
-            ans = Math.max(ans,width*ht);
-        }
-        return ans;
+        return maxArea;
     }
 }
