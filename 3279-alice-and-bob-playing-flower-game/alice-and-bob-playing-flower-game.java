3917083d053e31a6b301bcfedb6f9f507b59d17a
yashpanchal98 <pclyash@gmail.com>
2025-08-29 11:41:23 +0530

    Time: 1 ms (16.88%) | Memory: 41 MB (32.50%) - LeetSync

--- 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.java
+++ 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.java
@@ -1,13 +1,9 @@
 class Solution {
     public long flowerGame(int n, int m) {
         
-        int odd=0;
-        int even=0;
         long ans = 0;
-
-        if(m%2==0) odd = m/2;
-        else odd = m/2 + 1;
-        even = m/2;
+        int odd = (m+1)/2;
+        int even = m/2;
 
         for(int i=1; i<=n; i++){
             if(i%2==0) ans += odd;
