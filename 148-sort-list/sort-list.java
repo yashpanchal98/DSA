8ff7e7376db2bdab667ef27acc671f1727fb6f02
yashpanchal98 <pclyash@gmail.com>
2025-09-02 00:09:22 +0530

    Time: 11 ms (39.02%) | Memory: 57.1 MB (29.47%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -1,39 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
 
-    public ListNode findMid(ListNode head){
+    public ListNode findMid(ListNode head) {
         ListNode slow = head;
-        ListNode fast = head.next;
-        while(fast!=null && fast.next != null){
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null && fast.next.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
         return slow;
     }
 
-    public ListNode merge(ListNode RH, ListNode LH){
-
-        ListNode curr1 = RH;
-        ListNode curr2 = LH;
-        ListNode newNode = new ListNode();
+    public ListNode merge(ListNode leftHead, ListNode rightHead) {
+        ListNode curr1 = leftHead;
+        ListNode curr2 = rightHead;
+        ListNode newNode = new ListNode(-1);
         ListNode temp = newNode;
 
-        while(curr1!=null && curr2!=null){
-            if(curr1.val>=curr2.val){
-                temp.next = curr2;
-                curr2 = curr2.next;
-            } else {
+        while (curr1 != null && curr2 != null) {
+            if (curr1.val <= curr2.val) {
                 temp.next = curr1;
                 curr1 = curr1.next;
+            } else {
+                temp.next = curr2;
+                curr2 = curr2.next;
             }
-            temp = temp.next; 
+            temp = temp.next;
         }
 
-        while(curr1!=null){
+        while (curr1 != null) {
             temp.next = curr1;
             curr1 = curr1.next;
             temp = temp.next;
         }
-         while(curr2!=null){
+
+        while (curr2 != null) {
             temp.next = curr2;
             curr2 = curr2.next;
             temp = temp.next;
@@ -43,18 +54,16 @@ class Solution {
     }
 
     public ListNode sortList(ListNode head) {
-        if(head == null || head.next == null) 
-            return head;
+        if (head == null || head.next == null) return head;
 
         ListNode mid = findMid(head);
-        ListNode rightH = mid.next;
+        ListNode rightHead = mid.next;
         mid.next = null;
-        ListNode leftH = head;
+        ListNode leftHead = head;
 
+        ListNode sortLeft = sortList(leftHead);
+        ListNode sortRight = sortList(rightHead);
 
-        ListNode RH = sortList(rightH); 
-        ListNode LH = sortList(leftH);  
-
-        return merge(RH, LH);
+        return merge(sortLeft, sortRight);
     }
 }
