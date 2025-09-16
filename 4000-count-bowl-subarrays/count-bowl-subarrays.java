e0f835337b026c58891b91b16fce12766160ca53
yashpanchal98 <pclyash@gmail.com>
2025-09-16 17:36:32 +0530

    Time: 161 ms (30.82%) | Memory: 66.7 MB (39.59%) - LeetSync

---
+++ 4000-count-bowl-subarrays/count-bowl-subarrays.java
@@ -0,0 +1,61 @@
+class Solution {
+
+    public int[] nextGreater(int nums[]){
+
+        int n = nums.length;
+        int nxtGrtr[] = new int[n];
+        Stack<Integer> st = new Stack<>();
+
+        for(int i=n-1; i>=0; i--){
+
+            while(!st.isEmpty() && nums[st.peek()] <= nums[i]) st.pop();
+            if(st.isEmpty())  nxtGrtr[i] = -1;
+            else  nxtGrtr[i] = st.peek();
+            st.push(i);
+        }
+        return nxtGrtr;
+
+    }
+
+    public int[] prevGreater(int nums[]){
+
+        int n = nums.length;
+        int prevGrtr[] = new int[n];
+        Stack<Integer> st = new Stack<>();
+
+        for(int i=0; i<n; i++){
+
+            while(!st.isEmpty() && nums[st.peek()] <= nums[i]) st.pop();
+            if(st.isEmpty())  prevGrtr[i] = -1;
+            else  prevGrtr[i] = st.peek();
+            st.push(i);
+        }
+        return prevGrtr;
+
+    }
+
+    public long bowlSubarrays(int[] nums) {
+        
+        int n = nums.length;
+        int nxtGrtr[] = nextGreater(nums);
+        int prevGrtr[] = prevGreater(nums);
+
+        long ans = 0;
+
+        for(int i=1; i<n; i++){
+
+            int val1 = -1,val2=-1;
+            if(nxtGrtr[i] != -1){
+                val1 = nums[nxtGrtr[i]];
+            }
+            if(prevGrtr[i] != -1){
+                val2 = nums[prevGrtr[i]];
+            }
+
+            if(val1 > nums[i] && val2 > nums[i]){
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
