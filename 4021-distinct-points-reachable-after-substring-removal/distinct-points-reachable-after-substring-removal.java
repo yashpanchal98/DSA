0edc09b07380b57f542008659c1e88a60d2efcc1
yashpanchal98 <pclyash@gmail.com>
2025-09-28 16:19:51 +0530

    Time: 114 ms (34.20%) | Memory: 54 MB (100.00%) - LeetSync

--- 4021-distinct-points-reachable-after-substring-removal/distinct-points-reachable-after-substring-removal.java
+++ 4021-distinct-points-reachable-after-substring-removal/distinct-points-reachable-after-substring-removal.java
@@ -4,63 +4,38 @@ class Solution {
         HashSet<String> hs = new HashSet<>();
         int n = s.length();
 
-        int xO = 0, yO = 0;
+        int x = 0, y = 0;
         for (int i = 0; i < n; i++) {
             char ch = s.charAt(i);
-            if (ch == 'U')
-                yO++;
-            else if (ch == 'D')
-                yO--;
-            else if (ch == 'L')
-                xO--;
-            else
-                xO++;
+            if (ch == 'U') y++;
+            else if (ch == 'D') y--;
+            else if (ch == 'L') x--;
+            else x++;
         }
 
         int left = 0;
-        int right;
-        int x = xO, y = yO;
-        // first window - cancelling the effect of dir
-        for (right = 0; right < k; right++) {
-            char ch = s.charAt(right);
-            if (ch == 'U')
-                y--;
-            else if (ch == 'D')
-                y++;
-            else if (ch == 'L')
-                x++;
-            else
-                x--;
-        }
-        hs.add(x + "," + y);
 
-        while (right < n) {
-            // adding the cancelled left effect
-            char leftChar = s.charAt(left);
-            if (leftChar == 'U')
-                y++;
-            else if (leftChar == 'D')
-                y--;
-            else if (leftChar == 'L')
-                x--;
-            else
-                x++;
-            
-            // cancelling upcoming window effect
+        for (int right = 0; right < n; right++) {
             char rightChar = s.charAt(right);
-            if (rightChar == 'U')
-                y--;
-            else if (rightChar == 'D')
-                y++;
-            else if (rightChar == 'L')
-                x++;
-            else
-                x--;
-            
-            hs.add(x + "," + y);
-            
-            left++;
-            right++;
+            // cancelling the effect of right directions
+            if (rightChar == 'U') y--;
+            else if (rightChar == 'D') y++;
+            else if (rightChar == 'L') x++;
+            else x--;
+
+            if (right - left + 1 > k) {
+                // adding back the left pointer directions
+                char leftChar = s.charAt(left);
+                if (leftChar == 'U') y++;
+                else if (leftChar == 'D') y--;
+                else if (leftChar == 'L') x--;
+                else x++;
+                left++;
+            }
+
+            if (right - left + 1 == k) {
+                hs.add(x + "," + y);
+            }
         }
         return hs.size();
     }
