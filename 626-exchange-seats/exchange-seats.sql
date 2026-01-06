8b7d55a8bf615d6a8eb2f05391b9c7dc9b5d046b
yashpanchal98 <pclyash@gmail.com>
2026-01-06 13:02:44 +0530

    Time: 372 ms (14.07%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -1,9 +1,12 @@
+# Write your MySQL query statement below
 SELECT 
+    
     CASE 
-        WHEN ID % 2 = 0 THEN ID - 1
-        WHEN ID % 2 != 0 AND ID = (SELECT COUNT(*) FROM SEAT) THEN ID
+        WHEN ID%2 = 0 THEN ID-1
+        WHEN ID%2 != 0 AND ID = (SELECT COUNT(*) FROM SEAT) THEN ID 
         ELSE ID + 1
-    END AS id,
-    student
+    END AS ID,
+    STUDENT
+
 FROM SEAT
-ORDER BY id;
+ORDER BY ID;
